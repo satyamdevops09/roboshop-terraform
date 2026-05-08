a9737ed0fc0d9d6dd885279cde8e56532615d94f
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-05-08 11:45:34 +0530

    terraform data

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -27,9 +27,7 @@ resource "aws_route53_record" "public" {
 }
 
 resource "null_resource" "ansible" {
-  triggers = {
-    always= var.env == null ? timestamp() : "false"
-  }
+  count   = var.env == null ? 0 : 1
   depends_on = [aws_route53_record.record]
   provisioner "remote-exec" {
     connection {
