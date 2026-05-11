46a85f939379503cad18b32fa490250e4837367e
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-05-11 19:48:27 +0530

    terraform data

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -38,7 +38,7 @@ resource "null_resource" "ansible" {
     }
     inline = [
       "sudo pip3.11 install ansible",
-      "ansible-pull -i localhost, -U https://github.com/satyamdevops09/roboshop-ansible roboshop.yml -e role_name=${var.name} -e token=${var.token}"
+      "ansible-pull -i localhost, -U https://github.com/satyamdevops09/roboshop-ansible roboshop.yml -e role_name=${var.name} -e token=${var.token} -e env=${var.env}"
     ]
 
   }
