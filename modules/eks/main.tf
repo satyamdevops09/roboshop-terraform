01107e2a5e00bfca46ec057a983f67c2cb6c4ce2
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-06-02 13:06:57 +0530

    make changes

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -1,7 +1,7 @@
 resource "aws_eks_cluster" "main" {
   name = "example"
   role_arn = aws_iam_role.cluster.arn
-  version  = "1.33"
+  version  = "1.34"
   vpc_config {
     subnet_ids = ["subnet-0fe0f06b92d7f459f","subnet-08141bdca2bafdb54"]
   }
