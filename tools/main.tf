631faa54cd0c366fb5bb1d64a3769b69d58d4ae6
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-06-02 13:06:31 +0530

    make changes

--- tools/main.tf
+++ tools/main.tf
@@ -1,10 +1,13 @@
-module "tools" {
-  for_each = var.tools
-  source = "../modules/ec2"
-  ami    = var.ami
-  instance_type = each.value["instance_type"]
-  name = each.key
-  zone_id = var.zone_id
-  token = var.token
-}
+# module "tools" {
+#   for_each = var.tools
+#   source = "../modules/ec2"
+#   ami    = var.ami
+#   instance_type = each.value["instance_type"]
+#   name = each.key
+#   zone_id = var.zone_id
+#   token = var.token
+# }
 
+module "eks" {
+  source = "../modules/eks"
+}
