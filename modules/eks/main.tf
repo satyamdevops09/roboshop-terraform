6a22cf2f3171bd840cd55f7e5397f19c52eb7736
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-07-27 17:30:05 +0530

    feat: implement IAM roles and policies for EKS cluster and node group

--- modules/eks/main.tf
+++ modules/eks/main.tf
@@ -3,52 +3,25 @@ resource "aws_eks_cluster" "main" {
   role_arn = aws_iam_role.cluster.arn
   version  = var.eks_version
   vpc_config {
-    subnet_ids = ["subnet-0140cdff0ddb3d2bf","subnet-0f57d136113c7abae"]
+    subnet_ids = var.subnet_ids
   }
 
 }
 
 
-resource "aws_iam_role" "example" {
-  name = "eks-node-group-example"
 
-  assume_role_policy = jsonencode({
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      }
-    }]
-    Version = "2012-10-17"
-  })
-}
 
-resource "aws_iam_role_policy_attachment" "example-AmazonEKSWorkerNodePolicy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.example.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEKS_CNI_Policy" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.example.name
-}
-
-resource "aws_iam_role_policy_attachment" "example-AmazonEC2ContainerRegistryReadOnly" {
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.example.name
-}
-
-resource "aws_eks_node_group" "example" {
+resource "aws_eks_node_group" "main" {
+  for_each = var.node_group
   cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "nodeg-1"
-  node_role_arn   = aws_iam_role.example.arn
-  subnet_ids      = ["subnet-0140cdff0ddb3d2bf","subnet-0f57d136113c7abae"]
+  node_group_name = each.key
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 5
-    min_size     = 1
+    desired_size = each.value[min_node]
+    max_size     = each.value[max_node]
+    min_size     = each.value[min_node]
   }
 
   update_config {
---
+++ modules/eks/variables.tf
@@ -0,0 +1,4 @@
+variable "env" {}
+variable "eks_version" {}
+variable "subnet_ids" {}
+variable "node_group" {}
