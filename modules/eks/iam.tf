2a090fc45454bf4a5107ba0ab11a018abb69ce09
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-07-27 17:32:04 +0530

    fix: correct role reference for node IAM policy attachments

--- modules/eks/iam.tf
+++ modules/eks/iam.tf
@@ -65,15 +65,15 @@ resource "aws_iam_role" "node" {
 
 resource "aws_iam_role_policy_attachment" "node-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.main.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "node-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.main.name
+  role       = aws_iam_role.node.name
 }
 
 resource "aws_iam_role_policy_attachment" "node-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.main.name
+  role       = aws_iam_role.node.name
 }
