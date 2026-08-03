5433316586c0e5751552ab00ce49a7d16aa198f5
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-08-03 17:49:46 +0530

    feat: add IAM role resource for EC2 instances

---
+++ modules/ec2/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "main" {
+  name = "&{var.name}-&{var.env}-ec2-role"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "ec2.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        }
+    ]
+})
+
+ 
+}
