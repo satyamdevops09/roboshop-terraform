470204ef881de1f30bc1762f6736fa70b68cf435
nagasubbaraosatyamDevops <nagasubbaraosatyam@gmail.com>
2026-08-03 18:07:23 +0530

    feat: add iam_instance_profile to aws_instance resource

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "instances" {
   ami           = var.ami
   instance_type = var.instance_type
   vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+  iam_instance_profile = aws_iam_instance_profile.main.name
 
   root_block_device {
     volume_size = var.disk_size
